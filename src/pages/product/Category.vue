d20e8a000ca6ca54bf96004533b14ab6032d6210
NamDuongkiwi <namduongkiwi@gmail.com>
2021-07-15 16:06:01 +0700

    update products page

--- src/pages/product/Category.vue
+++ src/pages/product/Category.vue
@@ -1,20 +1,21 @@
 <template>
     <div class="category-container">
         <div class="sort">
-            <select>
-                <option>By price low to hight</option>
-                <option>By price hight to low</option>
+            <select @change="sortProducts($event)">
+                <option value="asc">By price low to hight</option>
+                <option value="desc">By price hight to low</option>
             </select>
         </div>
         <span class="text title">Categories</span>
-        <span class="text">All</span>
+        <span class="text" @click="setCategory($event, {})">All</span>
         <span class="text" v-for="category in categories" :key="category"
             @click="setCategory($event, category)">
             {{category.name}}</span>
 
         <div class="search">
-            <input type="text" placeholder="search..." />
-            <button type="submit">
+            <input type="text" placeholder="search..." v-model="searchKeyword"
+        @keypress.enter="searchProducts" />
+            <button type="submit" @click="searchProducts">
                 <img src="@/assets/icons/search-icon.png" />
             </button>
         </div>
@@ -25,9 +26,15 @@ import { mapState } from 'vuex'
 export default {
     name: 'Category',
 
+    data() {
+        return {
+            searchKeyword: "",
+        };
+    },
     computed:{
         ...mapState("products",[
             "categories",
+            "order"
         ])
     },
     methods:{
@@ -37,9 +44,23 @@ export default {
                 pageIndex: 1,
                 category
             });
+        },
+        searchProducts() {
+            this.$store.dispatch("products/getProducts", {
+                pageIndex: 1,
+                search: this.searchKeyword,
+            });
+        },
+        sortProducts(event){
+            console.log(event.target.value)
+            this.$store.dispatch("products/getProducts", {
+                pageIndex: 1,
+                order: event.target.value,
+            });
         }
     },
     created(){
+        this.searchKeyword = this.search;
         this.$store.dispatch("products/getCategories", {});
     }
 }
